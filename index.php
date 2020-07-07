b1548e35bf588cb270419fbe54062423675bd8f1
panjisob <sobaripanji8@gmail.com>
2020-07-07 09:05:50 +0700

    registrasi

--- index.php
+++ index.php
@@ -50,10 +50,13 @@ if(empty($_SESSION['namauser'])){
   <button class="navbar-toggler position-absolute d-md-none collapsed" type="button" data-toggle="collapse" data-target="#sidebarMenu" aria-controls="sidebarMenu" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
-  <input class="form-control form-control-dark w-100" type="text" placeholder="Search" aria-label="Search">
-  <ul class="navbar-nav px-3">
+  <!-- <input class="form-control form-control-dark w-100" type="text" placeholder="Search" aria-label="Search"> -->
+  <ul class="nav px-3">
     <li class="nav-item text-nowrap">
-      <a class="nav-link" href="logout.php">Sign out</a>
+      <a class="nav-link text-light" href="logout.php">Inbox</a>
+    </li>
+    <li class="nav-item text-nowrap">
+      <a class="nav-link text-light" href="logout.php">Sign out</a>
     </li>
   </ul>
 </nav>
@@ -68,7 +71,7 @@ if(empty($_SESSION['namauser'])){
           if ($_SESSION['role'] == "admin"){
         ?>
           <li class="nav-item">
-            <a class="nav-link active" href="#">
+            <a class="nav-link active" href="index.php?module=home">
               <span data-feather="home"></span>
               Home <span class="sr-only">(current)</span>
             </a>
@@ -80,7 +83,7 @@ if(empty($_SESSION['namauser'])){
             </a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="#">
+            <a class="nav-link" href="index.php?module=alternatif">
               <span data-feather="shopping-cart"></span>
               Alternatif
             </a>
@@ -103,7 +106,7 @@ if(empty($_SESSION['namauser'])){
         ?>
 
           <li class="nav-item">
-            <a class="nav-link active" href="#">
+            <a class="nav-link active" href="index.php?module=home">
               <span data-feather="home"></span>
               Home <span class="sr-only">(current)</span>
             </a>
@@ -177,6 +180,8 @@ if(empty($_SESSION['namauser'])){
       <?php 
         if ($_GET['module'] == 'home') {
           include "pages/home.php"; 
+        } elseif ($_GET['module'] == 'alternatif'){
+          include "pages/alternatif.php";
         } else {
           include "pages/home.php";
         }      
@@ -185,9 +190,10 @@ if(empty($_SESSION['namauser'])){
     </main>
   </div>
 </div>
-<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
-      <script>window.jQuery || document.write('<script src="vendor/bootstrap/js/jquery.slim.min.js"><\/script>')</script><script src="vendor/bootstrap/js/bootstrap.bundle.js"></script>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/feather-icons/4.9.0/feather.min.js"></script>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.7.3/Chart.min.js"></script>
-        <script src="js/dashboard.js"></script></body>
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/feather-icons/4.9.0/feather.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.7.3/Chart.min.js"></script>
+    <script src="js/dashboard.js"></script></body>
 </html>
---
+++ pages/alternatif.php
@@ -0,0 +1,24 @@
+
+<br>
+<ul class="nav nav-tabs" id="myTab" role="tablist">
+  <li class="nav-item">
+    <a class="nav-link active" id="home-tab" data-toggle="tab" href="#home" role="tab" aria-controls="home" aria-selected="true">Alternatif 1</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" id="profile-tab" data-toggle="tab" href="#profile" role="tab" aria-controls="profile" aria-selected="false">Alternatif 2</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" id="contact-tab" data-toggle="tab" href="#contact" role="tab" aria-controls="contact" aria-selected="false">Alternatif 3</a>
+  </li>
+</ul>
+<div class="tab-content" id="myTabContent">
+  <div class="tab-pane fade show active" id="home" role="tabpanel" aria-labelledby="home-tab">
+    Alternatif 1 cbasdjklcb  sldnbls sdlandlkasndln
+  </div>
+  <div class="tab-pane fade" id="profile" role="tabpanel" aria-labelledby="profile-tab">
+    Alternatif 2 asf msal s;dfm;asmcf scf;m;asmcf
+  </div>
+  <div class="tab-pane fade" id="contact" role="tabpanel" aria-labelledby="contact-tab">
+    Alternatif 3 cvsdklanklasn slfnas cjkbsfkbas
+  </div>
+</div>
